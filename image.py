fb764fe4bb282f411b65fb414299bf2da2a4658b
Nooria <samihamaysoonnooria@gmail.com>
2025-07-04 17:26:00 +0600

    nbv

--- image.py
+++ image.py
@@ -9,7 +9,7 @@ display_surface = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption('Image')
 
-image = pygame.image.load('squid-game-rules.jpg')
+image = pygame.image.load('player 222.jpg')
 DEFAULT_IMAGE_SIZE = (200,200)
 
 image = pygame.transform.scale(image, DEFAULT_IMAGE_SIZE)
